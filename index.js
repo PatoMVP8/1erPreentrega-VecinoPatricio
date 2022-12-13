7bbef8d491580fc5c1246561b933d7d8460fc10d
Patricio Vecino <patriciogabrielvecino151004@gmail.com>
2022-12-13 15:07:43 -0300

    cambios

--- index.js
+++ index.js
@@ -1,64 +1,55 @@
-function promediar(num1, num2, num3, num4, num5){
-let promedio = (num1 + num2 + num3 + num4 + num5)/5;
-     let mensaje = `El promedio de edad de la gente que ingreso a la web es de: ${promedio}`;
-     alert(mensaje);
- }
+  let entrada = prompt ("¿Es usted mayor de edad? Responda con Si/No".toLowerCase())
 
-//  let num1 = parseInt(prompt("Ingrese su edad"));
-//  if (num1 > 17){
-//     alert("eres mayor de edad, continua con la encuesta")
-//  }else{
-//     alert("eres menor de edad, recomendamos que llame a un adulto para proseguir")
-//  }
-//  let num2 = parseInt(prompt("Ingrese su edad"));
-//  if (num2 > 17){
-//     alert("eres mayor de edad, continua con la encuesta")
-//  }else{
-//     alert("eres menor de edad, recomendamos que llame a un adulto para proseguir")
-//  }
-//  let num3 = parseInt(prompt("Ingrese su edad"));
-//  if (num3 > 17){
-//     alert("eres mayor de edad, continua con la encuesta")
-//  }else{
-//     alert("eres menor de edad, recomendamos que llame a un adulto para proseguir")
-//  }
-//  let num4 = parseInt(prompt("Ingrese su edad"));
-//  if (num4 > 17){
-//     alert("eres mayor de edad, continua con la encuesta")
-//  }else{
-//     alert("eres menor de edad, recomendamos que llame a un adulto para proseguir")
-//  }
-//  let num5 = parseInt(prompt("Ingrese su edad"));
-//  if (num5 > 17){
-//     alert("eres mayor de edad, continua con la encuesta")
-//  }else{
-//     alert("eres menor de edad, recomendamos que llame a un adulto para proseguir")
-//  }
-//  promediar(num1, num2, num3, num4, num5);
-//  alert ("Los datos fueron completados exitosamente");
-
-
-
-
- let entrada = prompt ("¿Es usted mayor de edad?".toLowerCase())
-
- if(entrada === "no"){
-    alert("Realice la encuesta con un mayor de edad.")
+  if(entrada === "no"){
+      alert("Realice la encuesta con un mayor de edad.")
    
- }else{
-    function promediar(num1, num2, num3, num4, num5){
-       let promedio = (num1 + num2 + num3 + num4 + num5)/5;
-            let mensaje = `El promedio de edad de la gente que ingreso a la web es de: ${promedio}`;
-            alert(mensaje);}
-    let num1 = parseInt(prompt("Ingrese su edad"));
-    let num2 = parseInt(prompt("Ingrese su edad"));
-    let num3 = parseInt(prompt("Ingrese su edad"));
-    let num4 = parseInt(prompt("Ingrese su edad"));
-    let num5 = parseInt(prompt("Ingrese su edad"));
-    promediar(num1, num2, num3, num4, num5);
-    alert ("Los datos fueron completados exitosamente");
- }
+   }else if(entrada === "si"){
+      function promediar(num1, num2, num3, num4, num5){
+         let promedio = (num1 + num2 + num3 + num4 + num5)/5;
+              let mensaje = `El promedio de edad de la gente que ingreso a la web es de: ${promedio}`;
+             alert(mensaje);}
+      let num1 = parseInt(prompt("Ingrese su edad"));
+      alert (`El 1er participante tiene: ${num1}`);
+     let num2 = parseInt(prompt("Ingrese su edad"));
+     alert (`El 2do participante tiene: ${num2}`);
+     let num3 = parseInt(prompt("Ingrese su edad"));
+     alert (`El 3er participante tiene: ${num3}`);
+      let num4 = parseInt(prompt("Ingrese su edad"));
+      alert (`El 4to participante tiene: ${num4}`);
+      let num5 = parseInt(prompt("Ingrese su edad"));
+      alert (`El 5to participante tiene: ${num5}`);
+     promediar(num1, num2, num3, num4, num5);
+      alert ("Los datos fueron completados exitosamente");
+
+      const edades = [num1, num2, num3, num4, num5];
+ alert (`las edades presentadas fueron ${edades.join(", ")}.`)
+ alert (edades.sort((a,b)=> a-b));
+
+   }else{
+      alert("introduzca un dato valido")
+   }
+
+   
+
+       
+   
+   
+
+
+   
+
+
+ 
 
 
 
+
+   
+
+
+  
+ 
+
+
+ 
 
